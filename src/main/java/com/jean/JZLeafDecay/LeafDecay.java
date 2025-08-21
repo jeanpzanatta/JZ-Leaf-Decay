51e2bbdd4a8ab0aec98070673d0e1fb674ea3bde
Jean Pedro Zanatta <jeanpedrozanatta@gmail.com>
2025-08-21 11:37:05 -0400

    Início do projeto.

---
+++ src/main/java/com/jean/JZLeafDecay/LeafDecay.java
@@ -0,0 +1,17 @@
+package com.jean.JZLeafDecay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LeafDecay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
